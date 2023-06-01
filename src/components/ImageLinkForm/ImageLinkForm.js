dc5a6ce302f781630ba37a791de938c3c68f563f
Vlad Honcharenko <incrediblesmaug@gmail.com>
2023-06-01 12:35:49 +0300

    With working bg effects and new Rank Component

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,17 +1,18 @@
 import React from "react";
+import "./ImageLinkForm.css"
 
 
 
 const ImageLinkForm = () => {
     return  (
     <div>
-        <p className="text-xl mt-1">
+        <p className="text-xl m-10">
             {"Something weird will happen here if you download your picture here"}
         </p>
         <div className="center">
-        <div className="p-4 rounded shadow-md">
+        <div className="p-4 rounded shadow-md center gradient">
             <input className="text-xl p-1 w-3/5 m-2" type="text" name="" id="" />
-            <button className="transition text-xl w-2/5 transform" type="submit">Detect</button>
+            <button className=" text-white font-bold rounded text-xl w-2/5" type="submit">Detect</button>
         </div>
         </div>
         
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Rank = () => {
+  return (
+    <div>
+      <div className='m-3 text-xl text-white'>
+        {
+          "Your current rank is "
+        }
+      </div>
+      <div className='text-4xl text-white'>
+        {"#5"}
+      </div>
+    </div>
+  );
+};
+
+export default Rank;
